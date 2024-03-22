238553551343f20a363fd2cc4237c2ca18870db7
Tim Lehmenn <hi@timcreated.it>
2024-03-22 20:50:46 +0100

    feat: added wnma_flutter_package

--- wnma_dart_package copy/__brick__/{{project_name.snakeCase()}}/test/src/{{project_name.snakeCase()}}_test.dart
+++ wnma_dart_package copy/__brick__/{{project_name.snakeCase()}}/test/src/{{project_name.snakeCase()}}_test.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:{{project_name.snakeCase()}}/{{project_name.snakeCase()}}.dart';
 
 void main() {
