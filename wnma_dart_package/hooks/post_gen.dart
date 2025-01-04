775aa844578b3e986b0ce9346f1bfafe1bb1662b
Tim Lehmann <tim.lehmann@loql.com>
2025-01-04 11:58:05 +0100

    fix: update post_gen hooks to use project name for working directory in pub get and melos bootstrap

--- wnma_dart_package/hooks/post_gen.dart
+++ wnma_dart_package/hooks/post_gen.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
+  final name = context.vars['project_name'] as String;
   // Run `flutter packages get` after generation.
   final progress = context.logger.progress('Running pub get');
-  await Process.run('dart', ['pub', 'get']);
+  await Process.run('dart', ['pub', 'get'], workingDirectory: name);
   progress.complete();
 }
