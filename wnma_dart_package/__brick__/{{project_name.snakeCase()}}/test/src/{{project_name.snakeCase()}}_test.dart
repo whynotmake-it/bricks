f887fd98535a9e52886e745a64e479a82b788959
Tim Lehmann <hi@timcreated.it>
2025-09-30 09:57:57 +0200

    linter love

--- wnma_dart_package/__brick__/{{project_name.snakeCase()}}/test/src/{{project_name.snakeCase()}}_test.dart
+++ wnma_dart_package/__brick__/{{project_name.snakeCase()}}/test/src/{{project_name.snakeCase()}}_test.dart
@@ -1,3 +1,4 @@
+// We ignore prefer_const_constructors here to make testing more convenient.
 // ignore_for_file: prefer_const_constructors
 import 'package:test/test.dart';
 import 'package:{{project_name.snakeCase()}}/{{project_name.snakeCase()}}.dart';
@@ -5,7 +6,12 @@ import 'package:{{project_name.snakeCase()}}/{{project_name.snakeCase()}}.dart';
 void main() {
   group('{{project_name.pascalCase()}}', () {
     test('can be instantiated', () {
-      expect({{project_name.pascalCase()}}(), isNotNull);
+      expect(
+        {
+          {project_name.pascalCase()},
+        }(),
+        isNotNull,
+      );
     });
   });
 }
